e8ae16d6aaf4d83e850ecc84c8baf62a6ae4cf6d
joaoSouza-js <tygasouzaa@gmail.com>
2024-09-22 17:54:36 -0300

    (client) feat:  page  error illustration

--- client/src/components/page-error.tsx
+++ client/src/components/page-error.tsx
@@ -1,4 +1,5 @@
 import { Link, useRouteError } from "react-router-dom";
+import PageNotFoundIllustration from "@/assets/images/page-not-found-illustration.png";
 
 export function ErrorPage() {
     const error = useRouteError();
@@ -7,16 +8,21 @@ export function ErrorPage() {
     return (
         <div
             id="error-page"
-            className="flex flex-col gap-4 min-h-screen items-center justify-center"
+            className="flex flex-col gap-4 px-3 min-h-screen items-center justify-center"
         >
-            <h1 className="text-3xl text-zinc-50 font-bold">
+            <h1 className="text-3xl text-center text-zinc-50 font-bold">
                 Foi mal Campeão (404) 🥹🥹🥹
             </h1>
             <span className="text-xl text-zinc-50 font-bold">
-                Tente Recarregar á pagina
+                Tente Recarregar á página
             </span>
-            <Link className="text-lg underline text-primary" to={"/sign-in"}>
-                volte para de login
+            <img
+                src={PageNotFoundIllustration}
+                className="h-60 w-80"
+                alt="Ilustração de um gato descansando com uma pilha de livros ao se  lado "
+            />
+            <Link className="underline text-xl text-primary" to={"/sign-in"}>
+                volte para tela de login
             </Link>
         </div>
     );
